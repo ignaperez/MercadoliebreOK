7a9e2075dc64ff1b4838e1975e658adce932a791
Ignacio <ignaperez76@gmail.com>
2022-09-26 23:50:15 -0300

    Actualizacion

--- app.js
+++ app.js
@@ -11,3 +11,10 @@ app.listen(process.env.PORT || 3000
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
